42a3fb6a46b143bbd1320fce1150efbe2824cf43
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-09-29 17:14:49 +0100

    Added form tests

--- kikaihonyaku/tests.py
+++ kikaihonyaku/tests.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
 from .views import translate
+from .forms import SourceTextInputForm
 
 
 class TestUrls(SimpleTestCase):
@@ -23,11 +24,27 @@ class TestViews(SimpleTestCase):
         response = self.client.post(reverse("input"))
         self.assertTemplateUsed(response, "input.html")
 
+    """
     def test_output_page_template(self):
         response = self.client.post(reverse("output"))
         self.assertTemplateUsed(response, "output.html")
+    """
 
+class TestForms(SimpleTestCase):
 
+    def test_input_form_valid_data(self):
+        form = SourceTextInputForm(data={
+            "direction": "Ja>En",
+            "source_text": "ゼルダの伝説シリーズは任天堂のコンピュータゲームシリーズ。"
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_input_form_no_data(self):
+        form = SourceTextInputForm(data={})
+        # This form contains no data and therefore should not be valid
+        self.assertFalse(form.is_valid())
+        # There should be one error for each item of missing data
+        self.assertEquals(len(form.errors), 2)
 
 
 # use paramaterize to test the smaller helper functions
